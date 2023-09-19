7b283095950840b003dc7886837703c810a60b97
Laurent MEYER <laurent.meyer@viaformation.e-cdp.com>
2023-09-19 11:06:09 +0200

    base exercice

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Runtime.InteropServices;
-
-int A, B, C;
-A = 57;
-B = 24;
-C = A;
-A = B; 
-B = C;
-Console.WriteLine(A);
-Console.WriteLine(B);
-Console.WriteLine(C);
-
-Console.WriteLine("wesh alors")
-    
-    
+﻿
